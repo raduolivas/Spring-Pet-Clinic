c29fc587e1edbd3a646493f76cc3265b7737cd70
Rodolfo Silva <rosilva@fitec.org.br>
2018-08-21 14:22:46 -0300

    Initial Project Add

---
+++ src/main/java/com/bigtree/bigtreepetclinic/BigtreePetClinicApplication.java
@@ -0,0 +1,12 @@
+package com.bigtree.bigtreepetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigtreePetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BigtreePetClinicApplication.class, args);
+    }
+}
